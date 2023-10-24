065e3f77e784c578f20926c07bba330ad3365add
Dmitriy Kristya <fansocialfan@gmail.com>
2023-10-24 14:33:22 +0300

    First

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,132 @@
 package org.example;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
-  }
+public class MyList<T extends Number> implements Iterable<T> {
+    private Object[] array;
+    private int size;
+    private static final int CAPACITY = 10;
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList() {
+        array = new Object[CAPACITY];
+        size = 0;
+    }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
-  }
+    public void add(T element) {
+        resize();
+        array[size++] = element;
+        if (size == array.length) {
+            resize();
+        }
+    }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) array[index];
+    }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
-  }
+    private void resize() {
+        if (size == array.length) {
+            int newCapacity = array.length * 2;
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+    }
 
-  public int size() {
-    throw new RuntimeException("Not implemented");
-  }
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        T removedElement = (T) array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return removedElement;
+    }
+
+    public MyList<T> map(Function<T, T> f) {
+        MyList<T> mappedList = new MyList<>();
+        for (int i = 0; i < size; i++) {
+            T result = f.apply((T) array[i]);
+            mappedList.add(result);
+        }
+        return mappedList;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     *  * Inner class has access to all fields of an outer class.
+     *  * We can create it within the outer class.
+     *  *
+     *  * Inner static class has access to static fields of the outer class.
+     *  * We can create it without an outer class object.
+     */
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyListIterator();
+    }
+    private class MyListIterator implements Iterator<T> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (T) array[index++];
+        }
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        MyList<?> otherList = (MyList<?>) obj;
+        if (size != otherList.size) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!array[i].equals(otherList.array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public int hashCode() {
+        int result = 1;
+        for (int i = 0; i < size; i++) {
+            result = 31 * result + array[i].hashCode();
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(array[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -1,11 +1,224 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyListTest {
-  @Test
-  public void test() {
-    Assertions.assertTrue(true);
-  }
+    private MyList<Integer> myList;
+
+    @BeforeEach
+    public void createMyList() {
+        myList = new MyList<>();
+    }
+
+    @Test
+    public void testAddAndGet() {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        assertEquals(1, (int) myList.get(0));
+        assertEquals(2, (int) myList.get(1));
+        assertEquals(3, (int) myList.get(2));
+    }
+
+    @Test
+    public void testRemove() {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        int removed = myList.remove(1);
+
+        assertEquals(2, removed);
+        assertEquals(2, myList.size());
+    }
+
+    @Test
+    public void testMap() {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        Function<Integer, Integer> doubleFunction = x -> x * 2;
+        MyList<Integer> mappedList = myList.map(doubleFunction);
+
+        assertEquals(2, (int) mappedList.get(0));
+        assertEquals(4, (int) mappedList.get(1));
+        assertEquals(6, (int) mappedList.get(2));
+    }
+
+    @Test
+    public void testSize() {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        assertEquals(3, myList.size());
+    }
+
+    @Test
+    public void getThrowIndexOutOfBoundException() {
+
+        assertThrows(IndexOutOfBoundsException.class, () -> myList.get(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> myList.get(-1));
+    }
+
+    @Test
+    public void firstElementRemoval() {
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+
+        myList.remove(0);
+
+        assertEquals(2, myList.get(0));
+        assertEquals(4, myList.size());
+    }
+
+    @Test
+    public void lastElementRemoval() {
+
+        int count = 30;
+        for (int i = 0; i < count; i++) {
+            myList.add(i);
+        }
+        int previousNumber = myList.get(28);
+        //WHEN
+        myList.remove(29);
+        //THEN
+        assertEquals(previousNumber, myList.get(28));
+        assertEquals(count - 1, myList.size());
+    }
+
+    @Test
+    public void testEmptyMyListIterator() {
+        MyList<Integer> myList = new MyList<>();
+        Iterator<Integer> iterator = myList.iterator();
+
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void testListIterator() {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        Iterator<Integer> iterator = myList.iterator();
+
+        assertTrue(iterator.hasNext());
+        assertEquals(1, iterator.next());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(2, iterator.next());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(3, iterator.next());
+
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void testSameInstance() {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+
+        assertEquals(myList, myList);
+    }
+
+    @Test
+    public void testSameContent() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(1);
+        myList2.add(2);
+
+        assertEquals(myList1, myList2);
+    }
+
+    @Test
+    public void testDifferentContent() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(3);
+        myList2.add(4);
+
+        assertNotEquals(myList1, myList2);
+    }
+
+    @Test
+    public void testSameHashCodes() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(1);
+        myList2.add(2);
+
+        assertEquals(myList1.hashCode(), myList2.hashCode());
+    }
+
+    @Test
+    public void testDifferentHashCodes() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+        myList1.add(2);
+
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(3);
+        myList2.add(4);
+
+        assertNotEquals(myList1.hashCode(), myList2.hashCode());
+    }
+
+    @Test
+    public void testDifferentSize() {
+        MyList<Integer> myList1 = new MyList<>();
+        myList1.add(1);
+
+        MyList<Integer> myList2 = new MyList<>();
+        myList2.add(1);
+        myList2.add(2);
+
+        assertNotEquals(myList1, myList2);
+    }
+
+    @Test
+    public void testNullObject() {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+
+        assertNotEquals(null, myList);
+    }
+
+    @Test
+    public void testDifferentClassesSameContent() {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+
+        MyList<Double> differentClassList = new MyList<>();
+        differentClassList.add(1.0);
+        differentClassList.add(2.0);
+
+        assertNotEquals(myList, differentClassList);
+    }
 }
+
